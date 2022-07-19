a8c129a45c20293e3b7d9edf498d44c238f23b13
Tushar Srivastava <tsrivastava3011@gmail.com>
2022-07-19 22:45:30 +0530

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'       //typing rafce+enter give this boiler plate bcz of  "ES7 React/Redux/GraphQL/React-Native snippets" extension
+import Restaurant from './Components/Basics/restaurant'
+
+const App = () => {
+  return (
+    <Restaurant/>
+  )
+}
+
+export default App 
+
+
